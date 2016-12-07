8bbdaa05341860561a71636e9688e3c175039b08
unpairestgood <paulesingman@gmail.com>
2016-12-07 13:21:21 -0500

    update db to sqlalchemy orm

--- project/db_create.py
+++ project/db_create.py
@@ -1,19 +1,15 @@
-import sqlite3
-from _config import DATABASE_PATH
+from views import db
+from models import Task
+from datetime import date
 
-with sqlite3.connect('flasktaskr.db') as connection:
+# create the database and the db table
+db.create_all()
 
-    c = connection.cursor()
-
-    c.execute("""CREATE TABLE tasks(task_id INTEGER PRIMARY KEY AUTOINCREMENT,
-                name TEXT NOT NULL, due_date TEXT NOT NULL, priority INTEGER NOT NULL,
-                status INTEGER NOT NULL)""")
-
-    # insert dummy data into table
-    c.execute("""INSERT INTO tasks (name, due_date, priority, status)
-                VALUES("FINISH this tutorial", "03/25/2015", 10, 1)""")
-
-    c.execute("""INSERT INTO tasks (name, due_date, priority, status)
-                VALUES("FINISH REAL PYTHON COURSE 2", "02/25/2015", 10, 1)""")
+# insert data
+db.session.add(Task("Finish this tutorial",
+                    date(2015,3,13), 10, 1))
+db.session.add(Task("Finish Real Python",
+                    date(2015,4,13), 10, 1))
+db.session.commit()
 
 
